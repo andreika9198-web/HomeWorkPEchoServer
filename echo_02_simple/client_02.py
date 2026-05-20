fc130b9006d39673ed000a5ec0eb0c7e7f9f7d15
Sapranov Andrey <andreika9198@gmail.com>
2026-05-20 09:10:00 +0300

    Добавил комментария в файлах, папке echo_02

--- echo_02_simple/client_02.py
+++ echo_02_simple/client_02.py
@@ -2,32 +2,65 @@ import socket
 
 HOST = "127.0.0.1"
 PORT = 50432
-
+##С помощью socket.socket() создаём объект сокета
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    # Привязывает сокет к адресу и порту
     sock.connect((HOST, PORT))
+    print('=' * 50)
+    print('Информационная сноска')
+    print("Для выхода из чата введите 'EXIT' или 'ВЫЙТИ'")
+    print("Для остановки сервера введите 'STOP' или 'СТОП'")
+    print('=' * 50)
     while True:
         try:
+            #Пользователь вводит сообщение
             data_to_send = input('Ваше сообщение: ')
+            #Проверка на пустое сообщение или сообщение с одними пробелами, и
+            #заставляем пользователя повторно вводить сообщения
             if not  data_to_send.strip():
                 continue
+            #Полученные данные сохраняем верхнем регистре
             message = data_to_send.upper()
+            #Проверка на выход
             if  message == 'EXIT' or message == 'ВЫЙТИ':
+                #Отправляем сообщение на сервер
                 sock.sendall(data_to_send.encode())
+                #Получаем сообщение от сервера
                 response = sock.recv(1024)
                 print(f'Сервер: {response.decode()}')
                 break
+            #Преобразуем сообщения пользователя в байт
             data_bytes_to_send = data_to_send.encode()
+            # Отправляем сообщение на сервер
             sock.sendall(data_bytes_to_send)
+            #Сохраняем сообщение от сервера в переменную
             data_bytes_received = sock.recv(1024)
+            #Преобразуем сообщение из байтов в строку и сохраняем в переменную
             data_received = data_bytes_received.decode()
             print(f'Получено:', data_received)
         except  KeyboardInterrupt:
-            print("Пользователь вышел из чата")
+            print("\nПользователь вышел из чата")
+            #Ловим ошибку, и отправляем на сервер сообщение о выходе пользователя
             data_to_send = 'EXIT'
+            #Отправляем сообщение на сервер
             sock.sendall(data_to_send.encode())
+            #Получаем сообщение из сервера
             response = sock.recv(1024)
             print(f'Сервер: {response.decode()}')
             break
         except ConnectionAbortedError:
+            # """
+            #  Соглаcно условие дз:
+            #  дать возможность по команде клиента выключить
+            #  сервер, при этом клиент продолжает работу. Далее
+            #  клиент отправляет отключенному серверу какое-нибудь сообщение,
+            #  клиент не выпадает с ошибкой, а
+            #  выдает сообщение о разрыве подключение и
+            #  завершает работу с сообщением: “Process finished
+            #  with exit code 0”
+            #  """
             print("Программа на вашем хост-компьютере разорвала установленное подключение")
+            break
+        except ConnectionResetError:
+            print('Сервер, в данный момент отключен пользователем')
             break
--- echo_02_simple/server.py
+++ echo_02_simple/server.py
@@ -4,32 +4,44 @@ HOST = "127.0.0.1"
 PORT = 50432
 
 if __name__ == '__main__':
+    #С помощью socket.socket() создаём объект сокета
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serv_socket:
+        #Привязывает сокет к адресу и порту
         serv_socket.bind((HOST, PORT))
+        #Переводит сокет в режим ожидания подключений (серверный режим)
         serv_socket.listen()
+        #Флаг, указывает что сервер находиться в работе
         my_server = True
         while my_server:
             print('Ожидаю соединения...')
+            #Принимает входящее подключение (блокирующий вызов)
             sock, addr = serv_socket.accept()
+            #Автоматически закрывает сокет в конце блока
             with sock:
                 print('Подключение по адресу:', addr)
                 while True:
                     # Receive
                     try:
+                        #Получаем данные из сокета, 1024 — это размер буфера в байтах
                         data = sock.recv(1024)
                         if not data:
                             print(f'Клиент {addr} отключился')
                             break
+                        #Получаем сообщение от пользователя в байтах и преобразуем в строку
                         message = data.decode().strip().upper()
                         print(f'проверка {message}')
-
+                        #Отключаем пользователя от сервера, но сервер работает
                         if message  == 'EXIT' or message == 'ВЫЙТИ':
+                            #Отправляем данные на сервер
                             sock.sendall("Вы отключены от сервера. До свидания!".encode())
                             print(f'Клиент {addr} отключился по команде {message}')
                             break
+                        # Отключаем пользователя от сервера, но сервер работает
                         elif  message  == 'STOP' or message == 'СТОП':
+                            # Отправляем данные на сервер
                             sock.sendall("Вы отключили  сервер. До свидания, сервер!".encode())
                             print(f'Клиент {addr} отключил по команде {message}')
+                            #Флаг, указывает что сервер не нужно остановить
                             my_server = False
                             break
 
@@ -42,10 +54,12 @@ if __name__ == '__main__':
                         break
 
                     print(f'Получено: {data}, от {addr}')
+                    #Полученные данные сохраняем верхнем регистре
                     data = data.upper()
                     print(f'Отправлено: {data}, по адресу: {addr}')
 
                     try:
+                        #Отправляем данные на сервер
                         sock.sendall(data)
                     except ConnectionError:
                         print(f'Клиент внезапно отключился не могу отправить данные')
